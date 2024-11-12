026083dd08684081dcb155fb6f705e379c1965ce
iligiz <sulaimanoviligiz2000@gmail.com>
2024-11-12 11:08:46 +0300

    Первый коммит

---
+++ beta_security/src/main/java/com/alien/security/dto/UserModelDTO.java
@@ -0,0 +1,20 @@
+package com.alien.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModelDTO {
+
+    private int id;
+    private String name;
+    private String lastName;
+    private String middleName;
+    private String username;
+    private String role;
+}
